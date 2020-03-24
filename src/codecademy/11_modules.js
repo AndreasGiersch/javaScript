f2657956abf315e3733f9efc8e93b14e85e5655b
Andreas Giersch <agiersch@smail.uni-koeln.de>
2020-03-24 10:44:21 +0100

    updated on current progress

--- src/codecademy/11_modules.js
+++ src/codecademy/11_modules.js
@@ -4,15 +4,140 @@
 
 const Airplane = {}
 Airplane.myAirplane = 'StarJet'
-module.exports = Airplane
+
+// need to turn off other module exports, because export default at lesson "export defaults"
+// will be overridden
+
+// module.exports = Airplane
 
 /**
  * export modules II
  */
 
-module.exports = {
-    myAirplane: 'CloudJet',
-    displayAirplane: function() {
-        return this.myAirplane
+// module.exports = {
+//     myAirplane: 'CloudJet',
+//     displayAirplane: function() {
+//         return this.myAirplane
+//     },
+// }
+
+/**
+ * export defaults
+ */
+
+let availableAirplanesA = [
+    {
+        name: 'AeroJet',
+        fuelCapacity: 800,
+        availableStaff: [
+            'pilots',
+            'flightAttendants',
+            'engineers',
+            'medicalAssistance',
+            'sensorOperators',
+        ],
     },
+    {
+        name: 'SkyJet',
+        fuelCapacity: 500,
+        availableStaff: ['pilots', 'flightAttendants'],
+    },
+]
+
+const AirplaneExp = {
+    availableAirplanes: [availableAirplanesA[0], availableAirplanesA[1]],
 }
+
+// export default AirplaneExp
+
+/**
+ * named exports
+ */
+
+// let flightRequirements = { requiredStaff: 4 }
+
+// function meetsStaffRequirements(availableStaff, requiredStaff) {
+//     if (availableStaff.length >= requiredStaff) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+
+// export { availableAirplanes, flightRequirements, meetsStaffRequirements }
+
+/**
+ * export named exports as soon as they are declared
+ */
+
+export const availableAirplanes = [
+    {
+        name: 'AeroJet',
+        fuelCapacity: 800,
+        availableStaff: [
+            'pilots',
+            'flightAttendants',
+            'engineers',
+            'medicalAssistance',
+            'sensorOperators',
+        ],
+        maxSpeed: 1200,
+        minSpeed: 300,
+    },
+    {
+        name: 'SkyJet',
+        fuelCapacity: 500,
+        availableStaff: ['pilots', 'flightAttendants'],
+        maxSpeed: 800,
+        minSpeed: 200,
+    },
+]
+
+export const flightRequirements = {
+    requiredStaff: 4,
+    requiredSpeedRange: 700,
+}
+
+export function meetsStaffRequirements(availableStaff, requiredStaff) {
+    if (availableStaff.length >= requiredStaff) {
+        return true
+    } else {
+        return false
+    }
+}
+
+export function meetsSpeedRangeRequirements(
+    maxSpeed,
+    minSpeed,
+    requiredSpeedRange
+) {
+    const range = maxSpeed - minSpeed
+
+    if (range > requiredSpeedRange) return true
+    else return false
+}
+
+/**
+ * export as
+ */
+
+export {
+    availableAirplanes as aircrafts,
+    flightRequirements as flightReqs,
+    meetsStaffRequirements as meetsStaffReqs,
+    meetsSpeedRangeRequirements as meetsSpeedRangeReqs,
+}
+
+/**
+ * combining export statements
+ */
+
+export const bla = ' aaaa'
+
+export function blaa() {
+    console.log('bbbbb')
+}
+
+const obj = { prop: 'abc' }
+
+export default obj
--- src/codecademy/11_modulesImports.js
+++ src/codecademy/11_modulesImports.js
@@ -1,3 +1,5 @@
+import AirplaneExp from './11_modules'
+
 /**
  * require modules
  */
@@ -8,7 +10,10 @@ function displayAirplane() {
     console.log(Airplane.myAirplane)
 }
 
-displayAirplane()
+// need to turn off other module exports, because export default at lesson "export defaults"
+// will be overridden. Lines using these other module exports are commented out
+
+// displayAirplane()
 
 /**
  * require modules II
@@ -16,4 +21,91 @@ displayAirplane()
 
 const Airplane2 = require('./11_modules.js')
 
-console.log(Airplane2.displayAirplane())
+// console.log(Airplane2.displayAirplane())
+
+/**
+ * import
+ */
+
+function displayFuelCapacity() {
+    console.log(AirplaneExp)
+    AirplaneExp.availableAirplanes.forEach(element => {
+        console.log(`Fuel Capacity of ${element.name} :${element.fuelCapacity}`)
+    })
+}
+
+// displayFuelCapacity()
+
+/**
+ * named imports
+ */
+
+// import {
+//     availableAirplanes,
+//     flightRequirements,
+//     meetsStaffRequirements,
+// }
+
+function displayStaffStatus() {
+    availableAirplanes.forEach(function(element) {
+        console.log(
+            element.name +
+                ' meets staff requirements: ' +
+                meetsStaffRequirements(
+                    element.availableStaff,
+                    flightRequirements.requiredStaff
+                )
+        )
+    })
+}
+
+// displayStaffStatus()
+
+/**
+ * import named imports
+ */
+
+import {
+    availableAirplanes,
+    flightRequirements,
+    meetsStaffRequirements,
+    meetsSpeedRangeRequirements,
+} from './11_modules'
+
+function displaySpeedRangeStatus() {
+    availableAirplanes.forEach(function(element) {
+        console.log(
+            element.name +
+                ' meets speed range requirements: ' +
+                meetsSpeedRangeRequirements(
+                    element.maxSpeed,
+                    element.minSpeed,
+                    flightRequirements.requiredSpeedRange
+                )
+        )
+    })
+}
+
+// displaySpeedRangeStatus()
+
+/**
+ * import the "export as" exports
+ */
+
+// only need to change the used imports to the alias:
+// e.g. export { availableAirplanes as aircrafts }
+// --> import { aircrafts } from './11_modules';
+// aircrafts.forEach(...
+
+/**
+ * combining import statements
+ */
+
+import { bla, blaa } from './11_modules'
+
+import abc from './11_modules'
+
+console.log(bla)
+blaa()
+
+console.log(abc.prop)
